bc59acb4b928d17e51f1f7ca4a0133e7abb72476
Koi-AS <lightningblade.901@gmail.com>
2023-05-12 19:53:58 -0400

    I can render other pages other than the home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const home = require('./homePage.js');
+const dashboard = require('./dashboard.js')
 
 router.use('/', home);
+router.use('/dashboard', dashboard);
 
 module.exports = router;
