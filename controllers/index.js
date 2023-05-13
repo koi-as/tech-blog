0017e1bc023afe8c595c4d10a10e828e8b6506c4
Koi-AS <lightningblade.901@gmail.com>
2023-05-12 20:17:11 -0400

    Added a login route and setup a navbar

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const home = require('./homePage.js');
-const dashboard = require('./dashboard.js')
+const dashboard = require('./dashboard.js');
+const login = require('./login.js');
 
 router.use('/', home);
 router.use('/dashboard', dashboard);
+router.use('/login', login);
 
 module.exports = router;
