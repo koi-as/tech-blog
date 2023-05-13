8486303cb558f841e9bf81e88cb2651c59ddbc50
Koi-AS <lightningblade.901@gmail.com>
2023-05-13 12:04:35 -0400

    Added a signup page that you can navigate to

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const home = require('./homePage.js');
 const dashboard = require('./dashboard.js');
 const login = require('./login.js');
+const signup = require('./signup.js');
 
 router.use('/', home);
 router.use('/dashboard', dashboard);
 router.use('/login', login);
+router.use('/signup', signup);
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -14,19 +14,20 @@ User.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
+            autoIncrement: true
         },
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true
         },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
             validate: {
-                isEmail: true,
-            },
+                isEmail: true
+            }
         },
         password: {
             type: DataTypes.STRING,
